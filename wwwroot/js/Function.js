7bf4cc316866afe46039b6d92cb3b3a09b6bc805
MuhammadSalmanHaider <msalmanhaider572@gmail.com>
2026-07-27 22:11:07 +0500

    Add project files.

---
+++ wwwroot/js/Function.js
@@ -0,0 +1,18 @@
+﻿const menuLink = document.querySelector(".menu-link");
+
+const subMenu = document.querySelector(".sub-menu");
+
+menuLink.addEventListener("click", function () {
+
+    if (subMenu.style.display === "block") {
+
+        subMenu.style.display = "none";
+
+    }
+    else {
+
+        subMenu.style.display = "block";
+
+    }
+
+});
